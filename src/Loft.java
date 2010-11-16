19115b0a4ccd109b1130e901c5de37e0337fcca3
RobinKervadec <robin.kervadec@eleves.ec-nantes.fr>
2010-11-16 20:11:43 +0100

    LoftRobin

--- src/Loft.java
+++ src/Loft.java
@@ -1,29 +1,48 @@
 import java.util.ArrayList;
+import java.io.*;
 
+import java.util.List;
 
 public class Loft {
 
 	/**
 	 * @param args
 	 */
-	public int w;
-	public int h;
-	public ArrayList<Case> plateau;
+	protected int taille;
+	protected int jour;
+	protected Case[][] plateau;
+	protected List<Neuneu> participants;
+	protected List<Neuneu> morts;
+	protected List<Neuneu> nes;
+	protected Integer nombreNeuneuTotal;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Case getCase(int w, int h){
+		return plateau[w][h];
 	}
-	public void initialier(){
-		
-	}
-	public void approvisionner(){
+	
+	public void remplissageAleatoire(float ratio){
 		
 	}
 	public void regarderLeLoft(){
 		
 	}
+	public void add(Neuneu neuneu){
+		participants.add(neuneu);
+	}
+	protected void selectionNaturelle(){
+		Neuneu plusFaible = participants.get(0);
+		for (Neuneu neuneu:participants){
+			if (neuneu.getEnergie() < plusFaible.getEnergie())
+				plusFaible = neuneu;
+		}
+		System.out.println("A la fin du jour n°"+jour+", "+plusfaible.getNom()+" a été déclaré le Neuneu le plus faible du Loft.");
+		System.out.println("Pour cette raison, "+plusFaible.getNom()+" est invité à quitter le Loft.");
+		participants.remove(plusFaible);
+	}
 	
+	public String donnerNumeroPourNomNeuneu(){
+		++nombreNeuneuTotal;
+		return nombreNeuneuTotal.toString();
+	}
 	
-
 }
