7fc7e21ac436942569b41f00758c55151ef55527
RobinKervadec <robin.kervadec@eleves.ec-nantes.fr>
2010-11-17 00:16:19 +0100

    LoftRobin

--- src/Loft.java
+++ src/Loft.java
@@ -9,21 +9,64 @@ public class Loft {
 	protected int jour = 0;
 	protected Case[][] plateau;
 	protected List<Neuneu> participants;
+	protected List<Nonneuneu> aliments;
 	protected List<Neuneu> morts;
 	protected List<Neuneu> nes;
 	protected Integer nombreNeuneuTotal;
 
+	public Loft(int tailleLoft, ZoneGraphique Zone) {
+		this.taille = tailleLoft;
+	}
+
 	public Case getCase(int w, int h) {
+		// Prise en compte des limites du loft dans le cas du deplacement du
+		// neuneu erratique
+		if (w <= 0) {
+			w = 0;
+		} else if (w >= taille) {
+			w = taille - 1;
+		}
+		if (w <= 0) {
+			h = 0;
+		} else if (h >= taille) {
+			h = taille - 1;
+		}
 		return plateau[w][h];
 	}
 
+	public List<Neuneu> getParticipants() {
+		return this.participants;
+	}
+
+	public List<Nonneuneu> getAliments() {
+		return this.aliments;
+	}
+
 	public void remplissageAleatoire(float ratio) {
-		int nombreNonneuneu = (int) (taille*taille*ratio)
-		for (int i = nombre)
+		int nombreNonneuneu = (int) (taille * taille * ratio);
+		Nonneuneu nonneuneu;
+		for (int i = 0; i <= nombreNonneuneu - 1; i++) {
+			int w = (int) (Math.random() * (taille - 1));
+			int h = (int) (Math.random() * (taille - 1));
+			int a = (int) (Math.random() * 2);
+			switch (a) {
+			case 0:
+				nonneuneu = new Carotte();
+				break;
+			case 1:
+				nonneuneu = new Viande();
+				break;
+			case 2:
+				nonneuneu = new Alcool();
+				break;
+			default:
+				nonneuneu = new Alcool();
+				break;
+			}
+		}
 	}
 
 	public void regarderLeLoft() {
-
 	}
 
 	public void add(Neuneu neuneu) {
@@ -36,11 +79,11 @@ public class Loft {
 			if (neuneu.getEnergie() < plusFaible.getEnergie())
 				plusFaible = neuneu;
 		}
-		System.out.println("A la fin du jour n°" + jour + ", "
+		System.out.println("A la fin du jour numero" + jour + ", "
 				+ plusFaible.getNom()
-				+ " a été déclaré le Neuneu le plus faible du Loft.");
+				+ " a ete declare le Neuneu le plus faible du Loft.");
 		System.out.println("Pour cette raison, " + plusFaible.getNom()
-				+ " est invité à quitter le Loft.");
+				+ " est invite à quitter le Loft.");
 		participants.remove(plusFaible);
 	}
 
@@ -52,7 +95,7 @@ public class Loft {
 	public void go(int nbJour) {
 		for (int i = 0; i <= nbJour - 1; i++) {
 			jour = jour + 1;
-			System.out.println("Début du jour n°" + jour + ".");
+			System.out.println("Debut du jour numero" + jour + ".");
 			for (Neuneu neuneu : participants) {
 				neuneu.tour();
 			}
@@ -60,4 +103,13 @@ public class Loft {
 		}
 	}
 
+	public void jeSuisMort(Neuneu neuneuMort) {
+		participants.remove(neuneuMort);
+		morts.add(neuneuMort);
+	}
+	
+	public void jeSuisNe(Neuneu jeuneNeuneu) {
+		participants.add(jeuneNeuneu);
+		nes.add(jeuneNeuneu);
+	}
 }
